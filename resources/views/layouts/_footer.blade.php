0976a6607322b15aceecd758ca43c29832ef3cf2
gaohang <isaackao@foxmail.com>
2020-05-05 16:33:53 +0800

    分离header与footer子视图组件

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+  <img class="brand-icon" src="https://cdn.learnku.com/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png">
+  <a href="https://learnku.com/laravel/courses" target=_blank>
+    practice makes perfect
+  </a>
+
+  <div class="float-right">
+    <a href="/about" >关于DEMO</a>
+  </div>
+</footer>
